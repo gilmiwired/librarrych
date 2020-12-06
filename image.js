8925a81450abdf53fcbe015b52e7005d05d73236
gilmi <65973449+gilmiwired@users.noreply.github.com>
2020-12-06 23:52:35 +0900

    v

--- image.js
+++ image.js
@@ -1,4 +1,4 @@
-ar fs = WScript.CreateObject("Scripting.FileSystemObject");
+var fs = WScript.CreateObject("Scripting.FileSystemObject");
 var file = fs.OpenTextFile("data.txt", 1);
 var str = file.ReadLine();
 var img = new Image();
