d0c56dd451a7acd2315c13f8e126898f4879e5c6
gilmi <65973449+gilmiwired@users.noreply.github.com>
2020-12-06 23:35:22 +0900

    Update image.js

--- image.js
+++ image.js
@@ -1,4 +1,5 @@
+ar fs = WScript.CreateObject("Scripting.FileSystemObject");
 var file = fs.OpenTextFile("data.txt", 1);
 var str = file.ReadLine();
-var img = new Image(); 
+var img = new Image();
 img.src = str + '.png';
