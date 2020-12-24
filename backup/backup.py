5fee312a8abdcabac7727aafb75ed569963ee8a6
gilmi <65973449+gilmiwired@users.noreply.github.com>
2020-12-25 03:59:39 +0900

    backup作成

--- backup/backup.py
+++ backup/backup.py
@@ -6,20 +6,24 @@ import subprocess
 from multiprocessing import Process
 
 def run_proc():
-    subprocess.call(["fswebcam","top.jpg","-q"])
-    subprocess.call(["bash","commit.sh"])
+    subprocess.call(["fswebcam","{}.jpg".format(b),"-q"])
 
 def daemonize():
     print('Parent process %s.' % os.getpid())
-    subprocess.call(["rm","-r","top.jpg"])
-    subprocess.call(["bash","commit.sh"])
-	p = Process(target=run_proc)
+    p = Process(target=run_proc)
     print('start child process.')
     p.start()
     p.join()
     print('end child process.')
+	if((b-n) < 200):
+		for i in range(n,(b-100)):
+			subprocess.call(["rm","-r","{}.jpg".format(i)])
+		n += 100
+b = 1
+n = 1
 
 if __name__ == '__main__':
     while True:
         daemonize()
-        time.sleep(20)
+        b += 1
+        time.sleep(900)
